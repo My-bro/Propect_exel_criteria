98708bda23cf42ddef1f874e1865cb2883f853f1
My-bro <axel.londas@epitech.eu>
2025-08-27 13:23:11 +0200

    feat: add allowedDevOrigins to next.config for local development

--- frontend/app/next.config.ts
+++ frontend/app/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
+const nextConfig = {
+  // ...other config
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://0.0.0.0:3000"
+  ]
+}
 export default nextConfig;
